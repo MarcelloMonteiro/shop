f3875dbc6aac6d3dcb5c20628011ac162cf24213
marcelomonteiro1 <marcelomonteiro@inforgeneses.com.br>
2022-01-14 15:46:43 -0300

    finalizando edição e exclusão

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/models/produtc.dart';
 import 'package:shop/utils/app_routes.dart';
 
@@ -31,10 +33,35 @@ class ProductItem extends StatelessWidget {
               },
             ),
             IconButton(
-              onPressed: () {},
               icon: Icon(Icons.delete),
               color: Theme.of(context).errorColor,
-            )
+              onPressed: () {
+                showDialog<bool>(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: Text('Excluir Produto'),
+                    content: Text('Tem certeza?'),
+                    actions: [
+                      TextButton(
+                        child: Text('Não'),
+                        onPressed: () => Navigator.of(ctx).pop(false),
+                      ),
+                      TextButton(
+                        child: Text('Sim'),
+                        onPressed: () => Navigator.of(ctx).pop(true),
+                      ),
+                    ],
+                  ),
+                ).then((value) {
+                  if (value ?? false) {
+                    Provider.of<ProductList>(
+                      context,
+                      listen: false,
+                    ).removeProduct(product);
+                  }
+                });
+              },
+            ),
           ],
         ),
       ),
